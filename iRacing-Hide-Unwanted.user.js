88ed721673c7f8be655825bdec2751c8ad59434f
Fuzzwah <fuzzwah@github.com>
2020-06-04 10:45:24 +1000

    version bump

--- iRacing-Hide-Unwanted.user.js
+++ iRacing-Hide-Unwanted.user.js
@@ -4,7 +4,7 @@
 // @description   Allows hiding of series and forum sections
 // @include       *://members.iracing.com/jforum/forums/list.page
 // @include       *://members.iracing.com/membersite/member/*
-// @version       1.20.03.04.2
+// @version       1.20.06.04.1
 // @grant         none
 // @copyright     2019+, fuzzwah (https://github.com/fuzzwah)
 // @license       MIT; https://raw.githubusercontent.com/fuzzwah/iRacing-Hide-Unwanted/master/LICENSE
